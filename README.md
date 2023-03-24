98baed11d955aa259a0e9aad9747b6789ab5c499
alfredoizdev <alfredodevelop@gmail.com>
2023-03-24 13:30:27 -0500

    [main] added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App && Node js using doker compose].
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `docker-compose up`
+
+use [.env] variables of you own s3 bucket keys
+
+process.env.AWS_KEY
+process.env.BUCKET_NAME
+process.env.AWS_SECRET
--- server/src/app.ts
+++ server/src/app.ts
@@ -42,32 +42,3 @@ export default function startApolloServer() {
 	})
 }
  
-// export interface MyContext {
-// 	token: string
-// }
-
-// export async function startApolloServer() {
-// const app = express();
-// const httpServer = http.createServer(app);
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-	// const server = new ApolloServer<MyContext>({
-	// 	typeDefs,
-	// 	resolvers,
-	// });
-
-	// await server.start();
-	// app.use('/graphql',cors(),express.json(),expressMiddleware(server,{
-	// 	context: async ({ req }) => {
-			// Get the user token from the headers.
-			// const token = req.headers.authorization || '';
-			// Add the user to the context
-			// return { token };
-//   		},
-// 	}));
-
-// 	await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-// 	console.log(`🚀 Server ready at http://localhost:4000/`);
-
-// }
