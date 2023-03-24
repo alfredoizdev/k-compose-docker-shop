61a067b30fa4d3bd473b8ba59dc0400af801c9b3
alfredoizdev <alfredodevelop@gmail.com>
2023-03-24 08:14:27 -0500

    [main] s3 client move

--- server/src/aws/s3Client.ts
+++
@@ -1,8 +0,0 @@
-import AWS from 'aws-sdk'
-
-export const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_KEY,
-  params: { Bucket: process.env.BUCKET_NAME }, 
-  secretAccessKey: process.env.AWS_SECRET,
-  region: 'us-east-1',
-});
